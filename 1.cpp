8494a9eecdef9d95c7a17ac466632bd7bd6c0a03
yjwyyds1 <146413450+yjwyyds1@users.noreply.github.com>
2026-03-20 15:34:12 +0800

    C++ board

---
+++ 1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using ld = long double;
+using PII=pair<ll,ll>;
+using PIII=pair<int,pair<int,int>>;
+const ld ESP = 1e-10;
+const ld PI = acosl(-1);
+const int N=2e5+10;
+const int M=2e6+10;
+const int mod = 1000000007;
+// const int mod = 998244353;
+//随机化
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<> Tp(1, 100);
+// cout<<fixed<<setprecision(10);
+
+
+void solve(){
+    
+    
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int _=1;
+    cin>>_;
+    while(_--){
+        solve();
+    }
+    return 0;
+}
